857ed91591f1b05ac9d07fdb718560d1b8b84694
MattMannEC <mattmann.ec@gmail.com>
2024-08-27 10:37:05 +0200

    POC rag :     - Pass embedding function to chroma at instantiation     - Prompt in French

--- chroma_store.py
+++ chroma_store.py
@@ -1,16 +1,18 @@
 import chromadb
 from langchain_chroma import Chroma
 from chromadb import Collection, ClientAPI
+from langchain_huggingface import HuggingFaceEmbeddings
 
 # Create a client to connect to the ChromaDB server and collection
 chroma_client: ClientAPI = chromadb.HttpClient(host='localhost', port=8000)
 COLLECTION_NAME = "themis"
 collection: Collection = chroma_client.get_or_create_collection(name=COLLECTION_NAME)
-
+embeddings_model = HuggingFaceEmbeddings()
 # Create a Chroma instance
 chroma_store = Chroma(
+    collection_name=COLLECTION_NAME,
     client=chroma_client,
-    collection_name=COLLECTION_NAME
+    embedding_function=embeddings_model
 )
 
 
