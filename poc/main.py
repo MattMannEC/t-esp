047e95a159a7987e561bddbc39da53cfd980d927
MattMannEC <mattmann.ec@gmail.com>
2024-09-26 11:48:09 +0200

    Streaming works

--- poc/main.py
+++ poc/main.py
@@ -1,8 +1,10 @@
 from flask import Flask, Response, request
 import json
-from rag import run_rag
 import time
-
+import logging
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 from flask import Flask, render_template
 from flask_sse import sse
@@ -11,7 +13,7 @@ app = Flask(__name__)
 app.config["REDIS_URL"] = "redis://localhost"
 app.register_blueprint(sse, url_prefix='/stream')
 
-from langchain_core.output_parsers import StrOutputParser
+from langchain_core.output_parsers import SimpleJsonOutputParser
 from langchain_core.prompts import ChatPromptTemplate
 from chroma_store import chroma_store
 def format_docs(docs):
@@ -20,7 +22,7 @@ def format_docs(docs):
 
 from langchain.callbacks.manager import CallbackManager
 from langchain_core.callbacks import StreamingStdOutCallbackHandler
-from langchain_community.llms import Ollama
+from langchain_ollama import ChatOllama
 from langchain_core.runnables import RunnablePassthrough
 
 RAG_TEMPLATE = """
@@ -38,35 +40,24 @@ Répondez à la question suivante:
 {question}"""
 
 rag_prompt = ChatPromptTemplate.from_template(RAG_TEMPLATE)
-llm = Ollama(model="mistral:latest", verbose=True, callback_manager=CallbackManager([StreamingStdOutCallbackHandler()]))
+llm = ChatOllama(model="mistral:latest", verbose=True)
 chain = (
     RunnablePassthrough.assign(context=lambda input: format_docs(input["context"]))
     | rag_prompt
     | llm
-    | StrOutputParser()
 )
-print("Chain loaded")
+logger.info("Chain loaded")
 
 def run_rag(question):
     docs = chroma_store.similarity_search(question)
-    print("Context loaded")
-
-    # Run the chain
-    print("Running chain")
-    return chain.invoke({"context": docs, "question": question})
-
-
-# def generate_llm_response():
-#     result = run_rag("Je suis serveur dans un bar, est-ce que mon patron peut me forcer à faire des heures supp ?")
-#     yield f"data: {json.dumps(result)}\n\n"
+    logger.info("Running chain")
+    return chain.stream({"context": docs, "question": question})    
 
 @app.route('/simulate_llm')
 def simulate_llm():
     question = "Je suis serveur dans un bar, est-ce que mon patron peut me forcer à faire des heures supp ?"
     def generate():
-        result = run_rag(question)
-        yield f"data: {json.dumps(result)}\n\n"
+        for s in run_rag(question):
+            yield s.content
     return Response(generate(), mimetype='text/event-stream')
-
-
 
