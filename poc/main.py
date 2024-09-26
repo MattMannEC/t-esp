f9ae92c55d4f42d45257a3616f2c90f9d29a4c67
MattMannEC <mattmann.ec@gmail.com>
2024-09-26 11:51:23 +0200

    Clean

--- poc/main.py
+++ poc/main.py
@@ -2,28 +2,27 @@ from flask import Flask, Response, request
 import json
 import time
 import logging
-# Configure logging
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
-
 from flask import Flask, render_template
 from flask_sse import sse
-
-app = Flask(__name__)
-app.config["REDIS_URL"] = "redis://localhost"
-app.register_blueprint(sse, url_prefix='/stream')
-
 from langchain_core.output_parsers import SimpleJsonOutputParser
 from langchain_core.prompts import ChatPromptTemplate
 from chroma_store import chroma_store
-def format_docs(docs):
-    return "\n\n".join(doc.page_content for doc in docs)
-
-
 from langchain.callbacks.manager import CallbackManager
 from langchain_core.callbacks import StreamingStdOutCallbackHandler
 from langchain_ollama import ChatOllama
 from langchain_core.runnables import RunnablePassthrough
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+# Run app
+app = Flask(__name__)
+app.config["REDIS_URL"] = "redis://localhost"
+app.register_blueprint(sse, url_prefix='/stream')
+
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
 
 RAG_TEMPLATE = """
 Vous êtes un assistant qui rend le droit français plus accessible au grand public.
@@ -39,6 +38,7 @@ Répondez à la question suivante:
 
 {question}"""
 
+# Create RAG chain
 rag_prompt = ChatPromptTemplate.from_template(RAG_TEMPLATE)
 llm = ChatOllama(model="mistral:latest", verbose=True)
 chain = (
